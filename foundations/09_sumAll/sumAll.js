4aad8f3c45f9a023bb4ce6c3d6393826a0cd6d47
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-04 17:55:08 +0300

    Javascript foundations exercise 09 solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    isNum = typeof a === 'number' && typeof b === 'number';
+    isPos = a >= 0 && b >= 0;
+    isInt = Number.isInteger(a) && Number.isInteger(b);
+    
+    if (!isNum || !isPos || !isInt) return "ERROR";
 
+    let start = a;
+    let end = b;
+
+    // Make sure we're going always in the positive direction
+    if (b < a) {
+        [start, end] = [end, start];
+    }
+
+    let length = end - start + 1;
+    let result = Array(length).keys()
+                .map(n => n + start)
+                .reduce((sum, n) => sum + n, 0);
+    
+    console.log(result)
+    return result;
 };
 
 // Do not edit below this line
