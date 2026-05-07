bc5eb312a41950f76fcf19466c105d67c9a55dda
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-08 00:14:31 +0300

    Javascript foundations exercise 13 solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    stringArr = string
+                    .toLowerCase()
+                    .split('')
+                    .filter(item => /[a-z0-9]/.test(item));
+                    
+    return stringArr.join('') === stringArr.toReversed().join('');
 };
 
 // Do not edit below this line
