01410dd99308006af7642e205df77263ffa5ca19
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-08 01:10:59 +0300

    Javascript foundations exercise 14 solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+
+    let limit = num;
+    if (typeof num === 'string') limit = parseInt(num);
+    if (limit === 0) return 0;
+
+    let fib = 1;
+    let [nMinus1, nMinus2] = [1, 1];
+    console.log(nMinus1, nMinus2)
+
+    for (let i = 2; i < limit; i++) {
+        nMinus2 = nMinus1;
+        nMinus1 = fib;
+        fib = nMinus1 + nMinus2;
+        //console.log(fib)
+    }
+
+    return fib;
 
 };
 
