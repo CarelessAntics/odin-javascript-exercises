c5f1ba84df9ffffb8c42e567c533699ea27cca92
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-07 23:53:59 +0300

    Javascript foundations exercise 12 solution

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	// Create an array of sequential numbers
+  const arr = Array(num).keys().map(n => n + 1)
+  return multiply(arr);
 };
 
 // Do not edit below this line
