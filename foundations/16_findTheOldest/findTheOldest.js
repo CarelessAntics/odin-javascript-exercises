4d394011f5beac6c4486e16660395e97e923677d
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-08 02:03:03 +0300

    Javascript foundations exercise 16 solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const oldest = people.sort((a, b) => {
+        let ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        let ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        // descending
+        return ageB - ageA;
+    })[0];
 
+    return oldest;
 };
 
 // Do not edit below this line
